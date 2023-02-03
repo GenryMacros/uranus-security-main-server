be7e3683bf8b82355aaedb83f39bddd14c13ba3d
HenryMacros <ulti1235@gmail.com>
2023-02-03 22:03:14 +0200

    mssql database create script

---
+++ sql_scripts/user_only_create.sql
@@ -0,0 +1,45 @@
+CREATE TABLE Client(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	username VARCHAR(20),
+	is_deleted BIT
+)
+
+CREATE TABLE ClientSecret(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	client_id INT FOREIGN KEY REFERENCES  Client(id) ON DELETE CASCADE,
+	password_hash VARCHAR(64),
+	password_salt VARCHAR(10),
+	user_private VARCHAR(475),
+	user_public VARCHAR(116)
+)
+
+CREATE TABLE ClientPersonalData(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	client_id INT FOREIGN KEY REFERENCES Client(id) ON DELETE CASCADE,
+	user_first_name VARCHAR(15),
+	user_last_name VARCHAR(20)
+)
+
+CREATE TABLE ClientLocation(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	client_id INT FOREIGN KEY REFERENCES Client(id) ON DELETE CASCADE,
+	country VARCHAR(20),
+	city VARCHAR(20),
+	addr VARCHAR(20),
+	ind INT
+)
+
+CREATE TABLE ClientContact(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	client_id INT FOREIGN KEY REFERENCES Client(id) ON DELETE CASCADE,
+	email VARCHAR(20),
+	phone VARCHAR(15),
+	telegram VARCHAR(20)
+)
+
+CREATE TABLE Camera(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	client_id INT FOREIGN KEY REFERENCES Client(id) ON DELETE CASCADE,
+	device_name VARCHAR(10),
+	is_deleted BIT
+)
