653f48a0a70ce6c5b937e0cd949f725b2951c0e6
GenryMacros <ulti1235@gmail.com>
2023-02-05 19:59:22 +0200

    crypto lib change due to incompatibility with java

--- api/utils/secrets/secrets_handler.py
+++ api/utils/secrets/secrets_handler.py
@@ -2,14 +2,16 @@ import base64
 import hashlib
 import secrets
 
-from cryptography.exceptions import InvalidSignature
-from cryptography.hazmat.primitives import serialization as crypto_serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 
 
 class SecretsHandler:
+    KEY_SIZE = 2048
+
     @classmethod
     def calculate_hash(cls, hash_data: str, salt: str) -> str:
         hasher = hashlib.sha256()
@@ -19,35 +21,46 @@ class SecretsHandler:
 
     @classmethod
     def generate_new_key_pair(cls):
-        key_pair = rsa.generate_private_key(
-            backend=default_backend(),
-            public_exponent=65537,
-            key_size=512)
-        private_key = key_pair.private_bytes(
-            crypto_serialization.Encoding.PEM,
-            crypto_serialization.PrivateFormat.PKCS8,
-            crypto_serialization.NoEncryption())
-
-        public_key = key_pair.public_key().public_bytes(
-            crypto_serialization.Encoding.OpenSSH,
-            crypto_serialization.PublicFormat.OpenSSH)
+        random_generator = Random.new().read
+        key = RSA.generate(cls.KEY_SIZE, random_generator)
+        public_key = str(key.public_key().exportKey(format='PEM'), "utf-8") \
+            .replace('-----BEGIN PUBLIC KEY-----', '') \
+            .replace('-----END PUBLIC KEY-----', '') \
+            .replace('\n', '').replace(' ', '')
+        private_key = str(key.exportKey(format='PEM'), "utf-8")
         return public_key, private_key
 
     @classmethod
-    def is_signature_valid(cls, public_key: bytes, signature: bytes, message: bytes) -> bool:
-        pubkey = crypto_serialization.load_ssh_public_key(
-            public_key, backend=default_backend())
+    def is_signature_valid(cls, public_key: str, signature: bytes, message: bytes) -> bool:
+        pubkey_decoded = base64.b64decode(public_key)
+        rsa_pubkey = RSA.importKey(pubkey_decoded)
+        verifier = PKCS115_SigScheme(rsa_pubkey)
+        msg_hash = SHA256.new(message)
         try:
-            pubkey.verify(
-                base64.b64decode(signature),
-                message,
-                padding.PSS(
-                    mgf=padding.MGF1(hashes.SHA256()),
-                    salt_length=padding.PSS.MAX_LENGTH),
-                hashes.SHA256())
+            verifier.verify(msg_hash, signature)
             return True
-        except InvalidSignature:
+        except:
             return False
+
+    @classmethod
+    def sign_message(cls, message: bytes, private_key: str) -> str:
+        msg_hash = SHA256.new(message)
+        rsa_private_key = RSA.importKey(private_key)
+        signer = PKCS1_v1_5.new(rsa_private_key)
+        sig = signer.sign(msg_hash)
+        return base64.b64encode(sig).decode('utf-8')
+
+    @classmethod
+    def cut_public_key(cls, public_key: str) -> str:
+        return public_key.replace('-----BEGIN PUBLIC KEY-----', '') \
+            .replace('-----END PUBLIC KEY-----', '') \
+            .replace('\n', '').replace(' ', '')
+
+    @classmethod
+    def cut_private_key(cls, private_key: str) -> str:
+        return private_key.replace('-----BEGIN RSA PRIVATE KEY-----', '') \
+            .replace('-----END RSA PRIVATE KEY-----', '') \
+            .replace('\n', '').replace(' ', '')
 
     @classmethod
     def generate_salt(cls):
